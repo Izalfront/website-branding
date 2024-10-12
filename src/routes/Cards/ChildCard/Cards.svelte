870162e201604ad8b8d4236723790f1d22fa3939
Muhammad Rizal Wahyudi <128357156+Izalfront@users.noreply.github.com>
2024-10-12 22:23:37 +0800

    wip: card content

---
+++ src/routes/Cards/ChildCard/Cards.svelte
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<div>
+	<div class="flex gap-6">
+		<div class="block">
+			<img class="w-full h-full" src="./card1.png" alt="" />
+			<div class="pt-6 text-xl">
+				<h1 class="font-bold">Crtly</h1>
+				<h1>Mobile App</h1>
+			</div>
+		</div>
+		<div class="block">
+			<img class="w-full h-full" src="./card2.png" alt="" />
+			<div class="pt-6 text-xl">
+				<h1 class="font-bold">Crtly</h1>
+				<h1>Mobile App</h1>
+			</div>
+		</div>
+		<div class="block">
+			<img class="w-full h-full" src="./card3.png" alt="" />
+			<div class="pt-6 text-xl">
+				<h1 class="font-bold">Crtly</h1>
+				<h1>Mobile App</h1>
+			</div>
+		</div>
+	</div>
+</div>
