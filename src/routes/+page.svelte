6cdb8d8a772bc6d8c2b59d57a93c5b74314828f3
Muhammad Rizal Wahyudi <128357156+Izalfront@users.noreply.github.com>
2024-10-17 14:38:37 +0800

    adjust: toggleMode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,9 +43,40 @@
 <div class="text-black bg-white dark:bg-black dark:text-white">
 	<button
 		on:click={toggleMode}
-		class="fixed left-0 bottom-8 transform -translate-x-1/2 px-4 py-2 rounded-full bg-gray-200 dark:bg-gray-800 dark:text-white shadow-lg transition-transform duration-300 hover:translate-x-0"
+		class="fixed left-0 bottom-8 transform -translate-x-1/2 hover:translate-x-0 p-2 rounded-full bg-gray-200 dark:bg-gray-800 shadow-lg transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+		aria-label={isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
 	>
-		Toggle {isDarkMode ? 'Light' : 'Dark'} Mode
+		{#if isDarkMode}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 text-yellow-400"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+				/>
+			</svg>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 text-gray-900"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+				/>
+			</svg>
+		{/if}
 	</button>
 	<div class="mx-10 md:mx-24 sm:mx-10">
 		<HomePage />
